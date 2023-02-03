af09e917e4c70e90fd2e70ebc49165b717946886
Anshu Singh <anshusingh2343@gmail.com>
2023-02-03 10:54:44 +0530

    first commit

---
+++ cir_area.py
@@ -0,0 +1,8 @@
+print("Calculate the Circumference and Area of a Circle")
+d=int(input('Enter the diameter of the circle:'))
+r=d/2
+c=2*3.145*r
+a=3.145*r**2
+print(f"Radius:{r}")
+print(f"Circumference : {c}")
+print(f"Area : {a}")
---
+++ si.py
@@ -0,0 +1,9 @@
+print("Calculate the simple interests")
+p=int(input("Enter the principal amount:"))
+r=int(input("Enter the rate:"))
+t=int(input("Enter the time:"))
+si=p*r*t/100
+print(f"Principal:{p}")
+print(f"Rate:{r}")
+print(f"Time:{t}")
+print(f"Simple Interest:{si}")
